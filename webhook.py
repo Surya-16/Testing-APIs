82460ce9716b8420c801f7d5f415515af796c2dd
Surya-16 <69412547+Surya-16@users.noreply.github.com>
2023-03-07 14:52:25 +0530

    Update webhook.py

--- webhook.py
+++ webhook.py
@@ -1,14 +1,33 @@
 from flask import Flask, request, Response
+import requests
+import json
 app = Flask(__name__)
-data=""
 @app.route('/my_webhook', methods=['POST'])
 def return_response():
     print(request.json);
-    data=request.json
     ## Do something with the request.json data.
     return Response(status=201)
 @app.route('/webhook', methods=['GET'])
 def get():
-    print(data)
-    return Response(status=200)
-if __name__ == "__main__": app.run()
+    print(request.json);
+    print(request.headers)
+    return Response(status=201)
+@app.route('/order', methods = ['GET'])
+def order():
+    orderid = request.args.get('order')
+    print(orderid)
+    url = "https://sandbox.cashfree.com/pg/orders/"+orderid
+    payload = ""
+    headers = {
+            'Accept': 'application/json',
+            'Content-Type': 'application/json',
+            'x-api-version': '2022-09-01',
+            'x-client-id': '{{AppId}}',
+            'x-client-secret': '{{SecretKey}}'
+              }
+    response = requests.request("GET", url, headers=headers, data=payload)
+    print(response.text)
+
+    return Response(response.text)
+
+if __name__ == "__main__": app.run(debug = True)
