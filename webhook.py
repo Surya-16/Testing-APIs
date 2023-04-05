5602781a6d86ec923f1f691079205da3071bffaf
Surya-16 <69412547+Surya-16@users.noreply.github.com>
2023-04-06 00:43:27 +0530

    Removed unwanted content
    Have removed unwanted content and introduced a piece of code that validates the webhook signature and returns an appropriate response.

--- webhook.py
+++ webhook.py
@@ -2,21 +2,31 @@ from flask import Flask, request, Response
 import requests
 import json
 app = Flask(__name__)
-@app.route('/my_webhook', methods=['POST'])
-def return_response():
-    print(request.json);
-    ## Do something with the request.json data.
-    return Response(status=201)
-@app.route('/webhook', methods=['GET'])
+
+#below /webhook endpoint is to validate the webhook signature
+@app.route('/webhook', methods=['GET','POST'])
 def get():
-    print(request.json);
-    print(request.headers)
-    return Response(status=201)
+    webhooksignature = request.headers["X-Webhook-Signature"]
+    ts =request.headers["X-Webhook-Timestamp"]
+    data = request.data
+    signatureData = ts+data.decode("utf-8")
+    message = bytes(signatureData, 'utf-8')
+    signature = base64.b64encode(hmac.new(b'SecretKey', message,digestmod=hashlib.sha256).digest()) #Replace your SecretKey
+    computed_signature=str(signature, encoding='utf8')
+    print("Webhook_Signature  :"+webhooksignature +"\n" + "Computed_Signature :" + computed_signature)
+    if(computed_signature==webhooksignature):
+        print("WEBHOOK_VALIDATION_SUCCESS")
+        return Response(status=200)
+    else:
+        print("WEBHOOK_VALIDATION_FAILED")
+        return Response(status=500)
+    
+ #below /order endpoint is for return_url   
 @app.route('/order', methods = ['GET'])
 def order():
     orderid = request.args.get('order')
     print(orderid)
-    url = "https://sandbox.cashfree.com/pg/orders/"+orderid
+    url = "https://sandbox.cashfree.com/pg/orders/"+orderid #PROD endpoint: https://api.cashfree.com/pg
     payload = ""
     headers = {
             'Accept': 'application/json',
@@ -27,7 +37,6 @@ def order():
               }
     response = requests.request("GET", url, headers=headers, data=payload)
     print(response.text)
-
     return Response(response.text)
 
 if __name__ == "__main__": app.run(debug = True)
