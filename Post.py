98d32451470ffd3688f26a301994a66210d39a77
Surya S <SuryaS@CFs-MacBook-Pro.local>
2024-03-06 22:33:36 +0530

    fix

--- Post.py
+++ Post.py
@@ -21,7 +21,7 @@ dummy_employees = [
     },
     {
         "id": 3,
-        "name": "Bob Johnson",
+        "name": "Bob Johnçson",
         "position": "Marketing Specialist",
         "salary": 55000.00,
         "department": "Marketing",
